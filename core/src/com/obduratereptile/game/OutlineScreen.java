1e4f7d4614eb94a7c4632303d63f1c124d65dafd
MarcLoyer <marcl40@comcast.net>
2017-10-27 06:10:02 -0700

    render to texture, then render to screen is working. Now to add the custom shader behavior

--- core/src/com/obduratereptile/game/OutlineScreen.java
+++ core/src/com/obduratereptile/game/OutlineScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -36,7 +37,7 @@ public class OutlineScreen implements Screen {
     public static final int FBO_SIZE = 1024;
     public static final float MAX_BLUR = 2f;
 
-    public OrthographicCamera cam;
+    public OrthographicCamera cam, cam2;
     public InputMultiplexer inputControllerMultiplexer;
     public Stage stage;
 
@@ -48,8 +49,8 @@ public class OutlineScreen implements Screen {
         inputControllerMultiplexer = new InputMultiplexer(stage);
         Gdx.input.setInputProcessor(inputControllerMultiplexer);
 
-        cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.setToOrtho(false);
+        cam = new OrthographicCamera();
+        cam2 = new OrthographicCamera();
 
         inputControllerMultiplexer = new InputMultiplexer(stage);
         Gdx.input.setInputProcessor(inputControllerMultiplexer);
@@ -99,7 +100,7 @@ public class OutlineScreen implements Screen {
 
         blurTargetA = new FrameBuffer(RGBA8888, tex.getWidth(), tex.getHeight(), false);
         blurTargetB = new FrameBuffer(RGBA8888, tex.getWidth(), tex.getHeight(), false);
-        fboRegion = new TextureRegion(blurTargetA.getColorBufferTexture());
+        fboRegion = new TextureRegion(blurTargetA.getColorBufferTexture(), tex.getWidth(), tex.getHeight());
         fboRegion.flip(false, true);
    }
 
@@ -116,6 +117,7 @@ public class OutlineScreen implements Screen {
         stage.act(delta);
 
         cam.update();
+        cam2.update();
         game.batch.setProjectionMatrix(cam.combined);
 //        batchL5.setProjectionMatrix(cam.combined);
 
@@ -124,81 +126,105 @@ public class OutlineScreen implements Screen {
 //        game.batch.draw(tex, 100, 100);
 //        game.batch.end();
 
-        // TODO: make orthoGestureController work with this code
-
-        //Start rendering to an offscreen color buffer
+        // debug: let's just draw to texture, then draw to the screen
+        resizeBatch(tex.getWidth(), tex.getHeight());
         blurTargetA.begin();
-
-        //Clear the offscreen buffer with an opaque background
-        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 0f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        //before rendering, ensure we are using the default shader
-        batchL5.setShader(null);
-
-        //resize the batch projection matrix before drawing with it
-//        resizeBatch(FBO_SIZE, FBO_SIZE);
-
-        //now we can start drawing...
         batchL5.begin();
-        batchL5.draw(tex, 0, 0); // render the texture without any blurring
+        batchL5.draw(tex,0,0);
+        batchL5.end();
         batchL5.flush();
         blurTargetA.end();
 
-        //now let's start blurring the offscreen image
-        batchL5.setShader(shader);
-
-        //since we never called batch.end(), we should still be drawing
-        //which means our blurShader should now be in use
-
-        //ensure the direction is along the X-axis only
-        shader.setUniformf("dir", 1f, 0f);
-
-        //update blur amount based on touch input
-        float mouseXAmt = Gdx.input.getX() / (float)Gdx.graphics.getWidth();
-        shader.setUniformf("radius", mouseXAmt * MAX_BLUR);
-
-        //our first blur pass goes to target B
-        blurTargetB.begin();
-        fboRegion.setTexture(blurTargetA.getColorBufferTexture());
-        batchL5.draw(fboRegion, 0, 0); //draw the scene to target B with a horizontal blur effect
-        batchL5.flush();
-        blurTargetB.end();
-
-        //now we can render to the screen using the vertical blur shader
-
-        //update our projection matrix with the screen size
-//        resizeBatch(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batchL5.setProjectionMatrix(cam.combined);
 
-        //update the blur only along Y-axis
-        shader.setUniformf("dir", 0f, 1f);
-
-        //update the Y-axis blur radius
-        float mouseYAmt = Gdx.input.getY() / (float)Gdx.graphics.getHeight();
-        shader.setUniformf("radius", mouseYAmt * MAX_BLUR);
-
-        //draw target B to the screen with a vertical blur effect
-        fboRegion.setTexture(blurTargetB.getColorBufferTexture());
-        batchL5.draw(fboRegion, 0, 0);
-
-        //reset to default shader without blurs
-        batchL5.setShader(null);
-
-        //finally, end the batch since we have reached the end of the frame
+        batchL5.begin();
+        batchL5.draw(tex, 100, 100);
         batchL5.end();
+
+        // TODO: make orthoGestureController work with this code
+
+//        //Start rendering to an offscreen color buffer
+//        blurTargetA.begin();
+//
+//        //Clear the offscreen buffer with an opaque background
+//        Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 0f);
+//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//
+//        //before rendering, ensure we are using the default shader
+//        batchL5.setShader(null);
+//
+//        //resize the batch projection matrix before drawing with it
+////        resizeBatch(FBO_SIZE, FBO_SIZE);
+//
+//        //now we can start drawing...
+//        batchL5.begin();
+//        batchL5.draw(tex, 0, 0); // render the texture without any blurring
+//        batchL5.flush();
+//        blurTargetA.end();
+//
+//        //now let's start blurring the offscreen image
+//        batchL5.setShader(shader);
+//
+//        //since we never called batch.end(), we should still be drawing
+//        //which means our blurShader should now be in use
+//
+//        //ensure the direction is along the X-axis only
+//        shader.setUniformf("dir", 1f, 0f);
+//
+//        //update blur amount based on touch input
+//        float mouseXAmt = Gdx.input.getX() / (float)Gdx.graphics.getWidth();
+//        shader.setUniformf("radius", mouseXAmt * MAX_BLUR);
+//
+//        //our first blur pass goes to target B
+//        blurTargetB.begin();
+//        fboRegion.setTexture(blurTargetA.getColorBufferTexture());
+//        batchL5.draw(fboRegion, 0, 0); //draw the scene to target B with a horizontal blur effect
+//        batchL5.flush();
+//        blurTargetB.end();
+//
+//        //now we can render to the screen using the vertical blur shader
+//
+//        //update our projection matrix with the screen size
+////        resizeBatch(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//        batchL5.setProjectionMatrix(cam.combined);
+//
+//        //update the blur only along Y-axis
+//        shader.setUniformf("dir", 0f, 1f);
+//
+//        //update the Y-axis blur radius
+//        float mouseYAmt = Gdx.input.getY() / (float)Gdx.graphics.getHeight();
+//        shader.setUniformf("radius", mouseYAmt * MAX_BLUR);
+//
+//        //draw target B to the screen with a vertical blur effect
+//        fboRegion.setTexture(blurTargetB.getColorBufferTexture());
+//        batchL5.draw(fboRegion, 0, 0);
+//
+//        //reset to default shader without blurs
+//        batchL5.setShader(null);
+//
+//        //finally, end the batch since we have reached the end of the frame
+//        batchL5.end();
 
         stage.draw();
     }
 
+    public float worldWidth = 1000;
+
     @Override
     public void resize(int width, int height) {
         stage.getViewport().update(width, height, true);
+
+        resizeCamera(width, height);
+    }
+
+    void resizeCamera(float width, float height) {
+        cam.setToOrtho(false, worldWidth, worldWidth * height / width);
     }
 
     void resizeBatch(int width, int height) {
-        cam.setToOrtho(false, width, height);
-        batchL5.setProjectionMatrix(cam.combined);
+        worldWidth = cam.viewportWidth;
+        cam2.setToOrtho(false, width, height);
+        batchL5.setProjectionMatrix(cam2.combined);
     }
 
     void renderEntities(SpriteBatch batch) {
